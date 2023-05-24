bfa14140f64a27410f9c8f6657062703270a8c49
DenisFeofanov <omail4me@gmail.com>
2023-05-24 17:33:51 +0500

    clean css

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,9 +2,8 @@ const MovieList = props => {
   return (
     <>
       {props.movies.map((movie, index) => (
-          <div className="my-card">
-              <p>{movie.Title}</p>
-          <img src={movie.Poster} alt="movie"></img>
+        <div className="card">
+          <img className="card-image" src={movie.Poster} alt="movie"></img>
         </div>
       ))}
     </>
